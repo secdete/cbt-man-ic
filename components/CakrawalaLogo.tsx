b25bb91fe2f8ffd94cfde18161b03b33ad790f25
naufal jomok <naufalluthfi91@gmail.com>
2026-09-24 14:30:23 +0700

    fix(ui): add white circular backdrop to CakrawalaLogo for high contrast on dark backgrounds

--- components/CakrawalaLogo.tsx
+++ components/CakrawalaLogo.tsx
@@ -17,19 +17,27 @@ export default function CakrawalaLogo({
   const actualHeight = height ?? size;
 
   return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img
-      src={src}
-      alt="Logo Cakrawala"
-      style={{ maxHeight: `${actualHeight}px`, width: "auto" }}
-      className={`object-contain block ${className}`}
-      onError={() => {
-        if (src === "/logo-cakrawala.png") {
-          setSrc("/Logo Cakra.png");
-        } else if (src !== "/logo-cakrawala.svg") {
-          setSrc("/logo-cakrawala.svg");
-        }
+    <div
+      className="inline-flex items-center justify-center rounded-full bg-white p-1 shadow-xs border border-white/20 flex-shrink-0"
+      style={{
+        width: `${actualHeight + 6}px`,
+        height: `${actualHeight + 6}px`,
       }}
-    />
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={src}
+        alt="Logo Cakrawala"
+        style={{ maxHeight: `${actualHeight}px`, width: "auto" }}
+        className={`object-contain block rounded-full ${className}`}
+        onError={() => {
+          if (src === "/logo-cakrawala.png") {
+            setSrc("/Logo Cakra.png");
+          } else if (src !== "/logo-cakrawala.svg") {
+            setSrc("/logo-cakrawala.svg");
+          }
+        }}
+      />
+    </div>
   );
 }
